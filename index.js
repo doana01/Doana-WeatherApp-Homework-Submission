3a60e1adc9abb9f8acbebba0af69e9ad2a52141e
doana <artlovevintage@gmail.com>
2020-11-21 16:10:53 +0100

    Updated Main Icon
    .updated html image with icon
    .updated js to connect to api
    updated style in css

--- index.js
+++ index.js
@@ -71,6 +71,10 @@ function displayWeatherCondition(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#pressure").innerHTML = response.data.main.pressure;
+
+    let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 //
